633e7182c83f31f9f0cf39db9aae9b382d711c36
jikai.zhang <jikai.zhang@capgemini.com>
2019-06-25 10:14:38 +0800

    修改foo应用，测试通过bus总线refresh所有应用的配置

---
+++ foo-cloud-bus/src/main/java/com/foo/demo/web/ConfigController.java
@@ -0,0 +1,30 @@
+package com.foo.demo.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("config")
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @Value("${super.profile}")
+    private String sProfile;
+
+    @RequestMapping("profile")
+    public String getProfile() {
+
+        return profile;
+    }
+    @RequestMapping("sProfile")
+    public String getProfile1() {
+
+        return sProfile;
+    }
+}
