4b3989d2f479d104cb299950a7882e280e6ef226
jikai.zhang <jikai.zhang@capgemini.com>
2019-06-25 10:12:00 +0800

    添加ribbon负载均衡

--- customer-ribbon/src/main/java/com/customer/demo/web/UserController.java
+++ customer-ribbon/src/main/java/com/customer/demo/web/UserController.java
@@ -2,8 +2,11 @@ package com.customer.demo.web;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +20,19 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
-
     @GetMapping("getAdmin")
     public String getAdmin() {
         return restTemplate.getForObject("http://microservice-privoder/user/getAdmin", String.class);
     }
 
+    @HystrixCommand
     @GetMapping("getUser/{id}")
-    public String getAdmin(@PathVariable Long id) {
+    public String getUser(@PathVariable Long id) {
         User user = restTemplate.getForObject("http://microservice-privoder/user/getUser/{id}", User.class, id);
         return JSONObject.toJSONString(user);
+    }
+
+    public String fail() {
+        return "fail";
     }
 }
