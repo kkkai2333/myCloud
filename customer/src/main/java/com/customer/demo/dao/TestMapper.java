e16ff854c4293b05aca910b255afb112a96d3d6d
jikai.zhang <jikai.zhang@capgemini.com>
2019-06-25 10:09:42 +0800

    添加customer的测试方法

---
+++ customer/src/main/java/com/customer/demo/dao/TestMapper.java
@@ -0,0 +1,8 @@
+package com.customer.demo.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestMapper {
+}
---
+++ customer/src/main/java/com/customer/demo/web/TestController.java
@@ -0,0 +1,20 @@
+package com.customer.demo.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+
+    @GetMapping("customers")
+    public String customers() {
+
+
+        return null;
+    }
+}
