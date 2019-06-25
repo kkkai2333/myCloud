25dc151bffbdb58fb472f0f39227b526dadc117f
jikai.zhang <jikai.zhang@capgemini.com>
2019-06-25 10:11:02 +0800

    添加feign调用的负载均衡、开放健康状况接口

--- customer-feign/src/main/java/com/customer/demo/CustomerApplication.java
+++ customer-feign/src/main/java/com/customer/demo/CustomerApplication.java
@@ -2,10 +2,12 @@ package com.customer.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class CustomerApplication {
 
     public static void main(String[] args) {
